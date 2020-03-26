2745d8c4b9ed2598d1846a45a86b67383d63f845
rwheagle <rwh.kyler@gmail.com>
2020-03-26 11:03:52 -0600

    Update About.vue

--- front-end/src/views/About.vue
+++ front-end/src/views/About.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="about">
     <h1>About</h1>
-    <img src="/images/me.jpg"/>
+    <img src="/images/me.JPG"/>
     <p>I did this project for my religion class<br> This was a very fun project and I really enjoyed learning about the first vision</p>
   </div>
 </template>
